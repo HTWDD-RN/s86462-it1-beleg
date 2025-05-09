d25bddddd1f0764ea8ddcd732f1048a64f3017e8
Computer-Freak-2004 <89689115+Computer-Freak-2004@users.noreply.github.com>
2025-05-09 12:03:25 +0200

    better end screen, random questions & answers

--- docs/DevLog.md
+++ docs/DevLog.md
@@ -13,8 +13,19 @@
 - zusätzlich werden Antworten überprüft und das Ergebnis angezeigt
 - mit Media Queries gearbeitet, um Buttons auf kleinen Bildschirmen untereinander darzustellen
 - Todo: 
-  - Antwortmöglichkeiten random auf Buttons verteilen (jetzt ist immer klar das die erste Antwort richtig ist!)
-  - Einstellen des Themas (web, math, ...) über NavBar mit Radio Buttons
+  - Antwortmöglichkeiten random auf Buttons verteilen (jetzt ist immer klar das die erste Antwort richtig ist!) OK
+  - Buttons nach Auswahl der Antwort nicht klickbar machen OK
+  - Einstellen des Themas (web, math, ...) über NavBar mit Radio Buttons -> falls Thema anders als das aktuelle Thema, zurücksetzen
   - Button beim Anzeigen der Statistiken ausblenden/leeren & herausfinden wie man Zeilenumbruch für QuestionText machen kann (br?)
 - DevTools -> Network -> Disable Cache, damit immer akt. JS geladen wird
   - für json: {cache: "no-store"}
+
+  # 09.05.2025 (4h)
+
+  - Buttons werden nun deaktiviert, wenn Frage beantwortet
+  - Endbildschirm verbessert: 
+    - Buttons versteckt
+    - stattdessen wird genauere Statistik angzeigt mit jeder Aufgabe
+  - Antworten werden zufällig auf Buttons verteilt
+  - schicke Unicode-Symbole hinzugefügt
+  - Fragen werden nun nur am Anfang einer Runde geladen und auch gemischt
--- src/Lernprogramm.js
+++ src/Lernprogramm.js
@@ -9,39 +9,54 @@ document.addEventListener('DOMContentLoaded', function () {
     //p.setTask();
 });
 
+// Fisher–Yates shuffle
+function shuffleArray(array) { 
+    for (let i = array.length - 1; i >= 1; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+};
+
 // ############# Model ###########################################################################
 class Model {
     constructor() { 
+        this.questions = [];
         this.maxQuestions = 999999;
+        this.roundStarted = 0;
     }
 
     // Holt eine Frage aus dem Array, zufällig ausgewählt oder vom Server
     // es werden die IDs hardgecodet für jeden Aufgabentyp
     // z.B. 1-5 für Web, 6-10 für Mathe, ...
-    async getTask(nr) {
+    async getQuestion(nr) {
         // lokale Aufgaben
-        try{
-            const response = await fetch('./questions.json', {cache: "no-store"}); // no-store verhindert cache
-            const data = await response.json();
-            console.log(data);
-            
-            const questions = data.web // TODO: je nach Typ die richtigen Fragen auswählen
-            console.log(questions);
-            
-            // get maxQuestions im Thema
-            this.maxQuestions = questions.length;
+        if (this.roundStarted != 1){ // nur neuladen aus Datei, wenn neue Runde
+            try{
+                this.roundStarted = 1;
 
-            // gewünschte Frage laden
-            const question = questions[nr];
-            console.log("Frage:", question.q);
-            console.log("Antworten:", question.a);
-
-            return question; // Aufgabe + Lösungen
-        } catch (error) {
-            alert("Es muss wegen CORS diese Seite auf einem Server gehostet sein, damit JSON Daten geladen werden können!")
-            console.error('Fehler beim Laden der Daten:', error);
-            return null; // Fehlerbehandlung    
+                const response = await fetch('./questions.json', {cache: "no-store"}); // no-store verhindert cache
+                const data = await response.json();
+                
+                console.log(data.web)
+                shuffleArray(data.web) // TODO: je nach Typ die richtigen Fragen auswählen
+                this.questions = data.web;
+                console.log(this.questions);
+                
+                // get maxQuestions im Thema
+                this.maxQuestions = this.questions.length;
+            } catch (error) {
+                alert("Es muss wegen CORS diese Seite auf einem Server gehostet sein, damit JSON Daten geladen werden können!\nError: " + error)
+                console.error('Fehler beim Laden der Daten:', error);
+                return null; // Fehlerbehandlung    
+            }
         }
+
+        // gewünschte Frage laden
+        const question = this.questions[nr];
+        console.log("Frage:", question.q);
+        console.log("Antworten:", question.a);
+
+        return question; // Aufgabe + Lösungen
     }
 
     checkAnswer() {
@@ -53,10 +68,12 @@ class Model {
 // ############ Controller: Frage verarbeiten #########################################################
 class Presenter {
     constructor() {
-        this.questionNr = -1;
         this.question = null;
+        this.questionNr = -1;
         this.correctQuestions = 0;
-        this.questionAnswered = 0;
+
+        this.arrayCreated = 0;
+        this.questionLog = null; // -1: falsch, 0: unabeantwortet, 1: richtig | Ausgewähle Antwort als Text, 0 unbeantwortet
     }
 
     setModelAndView(m, v) {
@@ -64,32 +81,88 @@ class Presenter {
         this.v = v;
     }
 
-     // Holt eine neue Frage aus dem Model und setzt die View
+    // String für Endbildschirm mit Zusammenfassung der Ergebnisse
+    createQuestionSummaryStr(){
+        let summaryString = "";
+        for (let i = 0; i < this.questionLog.length; i++){
+            let correctionString = "";
+            let answerStatus = "";
+            let answerString = "";
+
+            if (this.questionLog[i][0] == 0){
+                answerStatus = "🚫";
+                answerString = "-";
+            }
+            if (this.questionLog[i][0] == -1){
+                answerStatus = "❌";
+                answerString = this.questionLog[i][1];
+                correctionString = " (richtig ist: " + this.m.questions[i].a[0] + ")";
+
+            }
+            if (this.questionLog[i][0] == 1){
+                answerStatus = "✅";
+                answerString = this.questionLog[i][1]
+            }
+
+            summaryString = summaryString + answerStatus + " " + (i+1) + ". " + this.m.questions[i].q + ": " + answerString + correctionString  + "<br>";
+        }
+        return summaryString;
+    }
+
+    // Holt eine neue Frage aus dem Model und setzt die View
     async setQuestion() {
         this.questionNr++;
-        this.questionAnswered = 0;
+        // Buttons deaktivieren
+        View.setQuestionButtonsDisabled(false);
+        View.setEndscreen("disabled");
 
         console.log(this.questionNr+1 + "/" + this.m.maxQuestions);
         if (this.questionNr+1 > this.m.maxQuestions){ // alle Fragen beantwortet?
             // Hier eine Übersicht anzeigen (wieviel richtig?)
-            View.renderQuestionText("Alle Fragen beantwortet!\nRichtig beantwortet: " + this.correctQuestions + " von " + this.m.maxQuestions);
+            let summaryString = this.createQuestionSummaryStr();
+            View.renderEndScreenText(summaryString);
+            View.renderStatusText( "✅ richtig | ❌ falsch | 🚫 unbeantwortet");
+            //console.log("SummaryStr: " + summaryString);
+
+            let percentageCorrect = Math.round(this.correctQuestions / this.m.maxQuestions * 100);
+            View.renderQuestionText("Alle Fragen beantwortet!<br>Richtig beantwortet: " + this.correctQuestions + " von " + this.m.maxQuestions + " (" + percentageCorrect + "%)");
+            View.setEndscreen("enabled");
 
             // zurücksetzen
+            this.m.roundStarted = 0;
+
             this.questionNr = -1; 
             this.correctQuestions = 0;
-            this.questionAnswered = 0;
+
+            this.arrayCreated = 0;
+            this.questionLog = null;
             return
         }
-        this.question = await this.m.getTask(this.questionNr);
+
+        this.question = await this.m.getQuestion(this.questionNr); // Frage bekommen
+        // Array für Spielerantworten aller Fragen erstellen
+        if (this.arrayCreated == 0){
+            this.questionLog = new Array(this.m.maxQuestions);
+            // vorfüllen mit 0
+            for (let i = 0; i < this.m.maxQuestions; i++){
+                this.questionLog[i] = [0,0];
+            }
+            this.arrayCreated = 1;
+        }
         
         View.renderQuestionText(this.question.q);
         View.renderStatsText("Frage: " + (this.questionNr +1) + "/" + this.m.maxQuestions);
         View.renderStatusText("Bitte eine Antwort auswählen!")
-    
+        
+        let shuffledAnswers = [...this.question.a]; // Kopie erstellen
+        shuffleArray(shuffledAnswers); // random
+        
+        console.log(shuffledAnswers);
+        
         for (let i = 0; i < 4; i++) {
-            let wert = this.question.a[i];
+            let text = shuffledAnswers[i];
             let pos = i;
-            View.inscribeButtons(i, wert, pos); // Tasten beschriften -> View -> Antworten
+            View.inscribeButtons(i, text, pos); // Tasten beschriften -> View -> Antworten
         }
     }
 
@@ -100,17 +173,20 @@ class Presenter {
 
         if (this.question.a[0] == document.querySelectorAll("#answer-btn > *")[answer].textContent){ // Aufgabentext == Buttontext?
             console.log("Richtige Antwort!");
-            View.renderStatusText("Richtige Antwort!");
+            View.renderStatusText("✅ Richtige Antwort!");
 
-            if (this.questionAnswered == 0){ // nur zählen falls noch nicht beantwortet
-                this.correctQuestions++;
-            }
+            this.correctQuestions++;
+            this.questionLog[this.questionNr] = [1, document.querySelectorAll("#answer-btn > *")[answer].textContent];
         } 
         else{
-            console.log("Falsche Antwort! Richtig wäre " + this.question.a[0]);
-            View.renderStatusText("Falsche Antwort! Richtig wäre " + this.question.a[0]);
+            console.log("Falsche Antwort! Richtig ist " + this.question.a[0]);
+            View.renderStatusText("❌ Falsche Antwort! Richtig ist " + this.question.a[0]);
+
+            this.questionLog[this.questionNr] = [-1, document.querySelectorAll("#answer-btn > *")[answer].textContent];
         }
-        this.questionAnswered = 1;
+        console.log("QUESTION ARRAY: " + this.questionLog);
+        // Buttons deaktivieren
+        View.setQuestionButtonsDisabled(true);
     }
 }
 
@@ -137,6 +213,28 @@ class View {
         document.querySelectorAll("#answer-btn > *")[i].textContent = text;
         document.querySelectorAll("#answer-btn > *")[i].setAttribute("number", pos);
     }
+
+    static setQuestionButtonsDisabled(disabled_bool){
+        for (let i = 0; i < 4; i++){
+            document.querySelectorAll("#answer-btn > *")[i].disabled = disabled_bool;
+        }
+    }
+
+    // Buttons & EndScreenText verstecken/anzeigen
+    static setEndscreen(action){
+        if (action === "enabled"){
+            for (let i = 0; i < 4; i++){
+                document.querySelectorAll("#answer-btn > *")[i].style.display = "none";
+            }
+            document.getElementById("end-screen-text").style.display = "block";
+        }
+        else if (action == "disabled"){
+            for (let i = 0; i < 4; i++){
+                document.querySelectorAll("#answer-btn > *")[i].style.display = "block";
+            }
+            document.getElementById("end-screen-text").style.display = "none";
+        }
+    }
     
     checkEvent(event) {
         console.log(event.type);
@@ -147,11 +245,16 @@ class View {
 
     static renderQuestionText(text) {
         let div = document.getElementById("question");
-       // div.textContent = text;
         let p = document.createElement("p");
         p.innerHTML = text;
         div.replaceChildren(p);
-        
+    }
+
+    static renderEndScreenText(text) {
+        let div = document.getElementById("end-screen-text");
+        let p = document.createElement("p");
+        p.innerHTML = text;
+        div.replaceChildren(p);
     }
 
     static renderStatsText(text) {
@@ -161,6 +264,6 @@ class View {
 
     static renderStatusText(text){
         let div = document.getElementById("status-text");
-        div.textContent = "-> " + text; // Ändert den Textinhalt
+        div.textContent = "➣ " + text; // Ändert den Textinhalt
     }
 }
